fb95ba840302cab65441d1d7156c387bd24727a7
Manuel <mfsilva@unimayor.edu.co>
2019-04-12 17:57:17 -0500

    algo de diseño

--- views/modulos/productos.php
+++ views/modulos/productos.php
@@ -1,34 +1,27 @@
     <div class="row" id="row1">
     <div class="col-md-12">
-        <div>
-        <div class="table-condensed">
-            <!-- TABLA PRODUCTOS-->
             <div id="tablaProductos" align="center">
               <h4>Productos</h4>
-              
                 <?php 
                 $datoProductos = ControladorProductos::ctrMostrarProductos();
                 //var_dump($datoProductos);
                 foreach ($datoProductos as $dato) {
-                    echo '<div class="col-md-3">
-                        <div>
-                            <img class="img-circle img-responsive" style="width:150px;height: 100px;" src="src/assets/productos/' . $dato["fotoProducto"].'" alt="imagen de '.$dato["nombreProducto"].'">
+                    echo '<div class="col-md-2" id="prodCliente">
+                        <div  id="prodClienteDivImagen">
+                            <img class="img-circle img-responsive" src="src/assets/productos/' . $dato["fotoProducto"].'" alt="imagen de '.$dato["nombreProducto"].'" id="prodClienteImagen">
                         </div>
                         <div>
-                            <h2>'.$dato["nombreProducto"].'</h2>
+                            <h3>'.$dato["nombreProducto"].'</h3>
                         </div>
                         <div>
-                            <h3>$'.$dato["precioProducto"].'</h3>
+                            <h4>$'.$dato["precioProducto"].'</h4>
                         </div><br>
                         </div>';
-                }
+                } 
                 ?>
-          
+                
             </div>
-            </div>
-             </div>
-             <!-- END SIMULACION -->
         </div>
-        </div>
+        </div><br>
     </div>
 </div>
